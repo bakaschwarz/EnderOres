63ab390492fc5998ccb4e3beeb25fd64b1b1e41c
Jonas Kuball <jkuball@tzi.de>
2015-03-31 20:39:04 +0200

    added portal particles to the ore

--- src/main/java/de/geratheon/enderores/block/BlockOreEnderOre.java
+++ src/main/java/de/geratheon/enderores/block/BlockOreEnderOre.java
@@ -1,7 +1,13 @@
 package de.geratheon.enderores.block;
 
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import de.geratheon.enderores.init.ModItems;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockOreEnderOre extends BlockOreEnderOres {
     public BlockOreEnderOre() {
@@ -9,5 +15,62 @@ public class BlockOreEnderOre extends BlockOreEnderOres {
         this.setBlockName("enderOre");
         this.setHardness(2.5F);
         this.setHarvestLevel("pickaxe", 2);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(World w, int x, int y, int z, Random rand) {
+        func_150186_m(w, x, y, z);
+    }
+
+    /*
+     *  Modified Redstone Ore Block Code
+     */
+    private void func_150186_m(World p_150186_1_, int p_150186_2_, int p_150186_3_, int p_150186_4_)
+    {
+        Random random = p_150186_1_.rand;
+        double d0 = 0.0625D;
+
+        for (int l = 0; l < 6; ++l)
+        {
+            double d1 = (double)((float)p_150186_2_ + random.nextFloat());
+            double d2 = (double)((float)p_150186_3_ + random.nextFloat());
+            double d3 = (double)((float)p_150186_4_ + random.nextFloat());
+
+            if (l == 0 && !p_150186_1_.getBlock(p_150186_2_, p_150186_3_ + 1, p_150186_4_).isOpaqueCube())
+            {
+                d2 = (double)(p_150186_3_ + 1) + d0;
+            }
+
+            if (l == 1 && !p_150186_1_.getBlock(p_150186_2_, p_150186_3_ - 1, p_150186_4_).isOpaqueCube())
+            {
+                d2 = (double)(p_150186_3_ + 0) - d0;
+            }
+
+            if (l == 2 && !p_150186_1_.getBlock(p_150186_2_, p_150186_3_, p_150186_4_ + 1).isOpaqueCube())
+            {
+                d3 = (double)(p_150186_4_ + 1) + d0;
+            }
+
+            if (l == 3 && !p_150186_1_.getBlock(p_150186_2_, p_150186_3_, p_150186_4_ - 1).isOpaqueCube())
+            {
+                d3 = (double)(p_150186_4_ + 0) - d0;
+            }
+
+            if (l == 4 && !p_150186_1_.getBlock(p_150186_2_ + 1, p_150186_3_, p_150186_4_).isOpaqueCube())
+            {
+                d1 = (double)(p_150186_2_ + 1) + d0;
+            }
+
+            if (l == 5 && !p_150186_1_.getBlock(p_150186_2_ - 1, p_150186_3_, p_150186_4_).isOpaqueCube())
+            {
+                d1 = (double)(p_150186_2_ + 0) - d0;
+            }
+
+            if (d1 < (double)p_150186_2_ || d1 > (double)(p_150186_2_ + 1) || d2 < 0.0D || d2 > (double)(p_150186_3_ + 1) || d3 < (double)p_150186_4_ || d3 > (double)(p_150186_4_ + 1))
+            {
+                p_150186_1_.spawnParticle("portal", d1, d2, d3, 0.0D, -0.5D, 0.0D);
+            }
+        }
     }
 }
