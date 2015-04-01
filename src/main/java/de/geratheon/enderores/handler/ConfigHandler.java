01690ecce8d5dd951f9f622a519faeff5c076beb
Jonas Kuball <jkuball@tzi.de>
2015-04-01 15:31:16 +0200

    default rarity changed

--- src/main/java/de/geratheon/enderores/handler/ConfigHandler.java
+++ src/main/java/de/geratheon/enderores/handler/ConfigHandler.java
@@ -26,7 +26,7 @@ public class ConfigHandler {
     private static void loadConfiguration() {
         generateEnderOre = config.getBoolean("generateEnderOres", Configuration.CATEGORY_GENERAL, true, "Set whether the mod should generate ender ore or not. (Protip: It should.)");
         maxVeinSizeEnderOre = config.getInt("maxVeinSizeEnderOre", Configuration.CATEGORY_GENERAL, 3, 0, 100, "Sets the maximum vein size for the ender ore.");
-        chancesToSpawnEnderOre = config.getInt("rarityEnderOre", Configuration.CATEGORY_GENERAL, 50, 1, 100, "Sets the rarity of the ender ore. (Note: 1 is absolutely rare and 100 is absolutely common.");
+        chancesToSpawnEnderOre = config.getInt("rarityEnderOre", Configuration.CATEGORY_GENERAL, 25, 1, 100, "Sets the rarity of the ender ore. (Note: 1 is absolutely rare and 100 is absolutely common.");
         minYEnderOre = config.getInt("minYEnderOre", Configuration.CATEGORY_GENERAL, 0, 0, 256, "Sets the minimum y value where ender ore can occur.");
         maxYEnderOre = config.getInt("maxYEnderOre", Configuration.CATEGORY_GENERAL, 32, 0, 256, "Sets the maximum y value where ender ore can occur.");
 
