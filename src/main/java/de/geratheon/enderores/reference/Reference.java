7c4e7fd4ad5cdc089574e033e7026c9a81331627
Jonas Kuball <jkuball@tzi.de>
2015-04-01 12:05:16 +0200

    1.0 -> 1.0-BETA

--- src/main/java/de/geratheon/enderores/reference/Reference.java
+++ src/main/java/de/geratheon/enderores/reference/Reference.java
@@ -3,7 +3,7 @@ package de.geratheon.enderores.reference;
 public class Reference {
     public static final String MOD_ID = "enderores";
     public static final String MOD_NAME = "EnderOres";
-    public static final String VERSION = "1.7.10-1.0";
+    public static final String VERSION = "1.7.10-1.0-BETA";
     public static final String GUI_FACTORY_CLASS = "de.geratheon.enderores.client.gui.GuiFactory";
     public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 }
